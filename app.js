06448126fcc0d3d59b046800075943bd5b15ab5a
jhonboysibuea <jhonboysibuea@gmail.com>
2020-08-31 07:46:13 +0700

    manambahkan view

--- app.js
+++ app.js
@@ -2,21 +2,38 @@ const express = require("express"); //expressJs library handle request response 
 /** * App Variables */
 const app = express();
 const port = process.env.PORT || "8081";
+var path = require('path');
 /** * Routes Definitions */
 
-//data json
-var jsonProduct = {
-  "status": 200,
-  "message": "succes",
-  "data": {
-    "productId": "1",
-    "productName": "omjebs"
-  }
-};
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+//setup public folder
+app.use(express.static('./public'));
+
+app.get('/', function (req, res) {
+  res.render('pages/homepage')
+});
+
+
 /** routes akan  http://localhost:80000/product */
 app.get("/product", function (req, res) {
-  res.status(200).json(jsonProduct);
+  //data json
+  var jsonProduct = {
+    "status": 200,
+    "message": "succes",
+    "data": [{
+      "productId": "1",
+      "productName": "omjebs"
+    }]
+  };
+
+  res.render('pages/product', {
+    productList: jsonProduct
+  })
 });
+
 /** * Server Activation */
 app.listen(port, () => {
   console.log(`Listening to requests on http://localhost:${port}`);
